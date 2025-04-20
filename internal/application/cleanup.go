8ad82236042f76777da0a58ce2e69d7c7094251e
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-21 07:40:34 +1000

    Added more logging

--- internal/application/cleanup.go
+++ internal/application/cleanup.go
@@ -3,12 +3,14 @@ package application
 import (
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/handler"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
 )
 
 func CleanupOldPlans(c *gin.Context) {
 	err := handler.RemoveOldPlans()
 
 	if err != nil {
+		zap.L().Error("Error removing old plans", zap.Error(err))
 		c.IndentedJSON(500, gin.H{"error": "Failed to remove old plans"})
 		return
 	}
