899457c324e56e43194d2b2386035addc106b27f
Matthew Casperson <matthewcasperson@gmail.com>
2025-04-23 07:49:35 +1000

    Extract the hostname from the url

--- internal/domain/logging/enhanced_logging.go
+++ internal/domain/logging/enhanced_logging.go
@@ -4,11 +4,20 @@ import (
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/environment"
 	"github.com/samber/lo"
 	"go.uber.org/zap"
+	"net/url"
 )
+
+func getHostname(server string) string {
+	parsedURL, err := url.Parse(server)
+	if err != nil {
+		return ""
+	}
+	return parsedURL.Hostname()
+}
 
 func IsEnhancedLoggingEnabled(server string) bool {
 	servers := environment.GetEnhancedLoggingInstances()
-	return lo.Contains(servers, server)
+	return lo.Contains(servers, getHostname(server))
 }
 
 // LogEnhanced will log a message only if the server that made the request is specifically configured
