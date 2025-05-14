1774ec005701f9a3b72f74376d7d614279aa6a26
Matthew Casperson <matthewcasperson@gmail.com>
2025-05-15 08:05:19 +1000

    Added more debugging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/application"
+	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/environment"
 	"go.uber.org/zap"
 	"os"
 )
@@ -14,6 +16,13 @@ func main() {
 	}
 
 	zap.L().Info("Current working directory: " + cwd)
+	zap.L().Info("Disable validation: " + fmt.Sprint(environment.DisableValidation()))
+	zap.L().Info("OPA executable: " + fmt.Sprint(environment.GetOpaExecutable()))
+	zap.L().Info("OPA policy path: " + fmt.Sprint(environment.GetOpaPolicyPath()))
+	zap.L().Info("Tofu executable: " + fmt.Sprint(environment.GetTofuExecutable()))
+	zap.L().Info("Disable setting binary execution flag: " + fmt.Sprint(environment.DisableMakeBinariesExecutable()))
+	zap.L().Info("Enhanced logging instance: " + fmt.Sprint(environment.GetEnhancedLoggingInstances()))
+	zap.L().Info("Port: " + fmt.Sprint(environment.GetPort()))
 
 	if err := application.StartServer(); err != nil {
 		zap.L().Error(err.Error())
