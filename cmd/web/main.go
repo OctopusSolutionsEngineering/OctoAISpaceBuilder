e3e482cfcced715e0fac6df640c3e4b485afda46
Matthew Casperson <matthewcasperson@gmail.com>
2025-05-15 07:02:42 +1000

    Added some debugging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,9 +3,18 @@ package main
 import (
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/application"
 	"go.uber.org/zap"
+	"os"
 )
 
 func main() {
+	cwd, err := os.Getwd()
+	if err != nil {
+		zap.L().Error(err.Error())
+		return
+	}
+
+	zap.L().Info("Current working directory: " + cwd)
+
 	if err := application.StartServer(); err != nil {
 		zap.L().Error(err.Error())
 	}
