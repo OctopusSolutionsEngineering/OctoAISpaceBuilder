7b83a1b4cc04da8ea8cfbe73faacc23577fffd96
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-08 06:08:01 +1000

    Refactor MakeExecutable middleware to remove redundant error handling

--- internal/application/middleware/set_executable.go
+++ internal/application/middleware/set_executable.go
@@ -1,13 +1,12 @@
 package middleware
 
 import (
-	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/application/responses"
+	"path/filepath"
+
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/environment"
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/execute"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"net/http"
-	"path/filepath"
 )
 
 // MakeExecutable is a middleware function that sets the executable permissions for the specified files.
@@ -19,22 +18,13 @@ func MakeExecutable(c *gin.Context) {
 	installDir, err := environment.GetInstallationDirectory()
 	if err != nil {
 		zap.L().Error("Failed to get the installation directory", zap.Error(err))
-		c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
 	}
 
 	if err := execute.MakeAllExecutable(filepath.Join(installDir, "binaries")); err != nil {
 		zap.L().Error("Failed to make binaries executable", zap.Error(err))
-		c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
 	}
 
 	if err := execute.MakeAllExecutable(filepath.Join(installDir, "provider")); err != nil {
 		zap.L().Error("Failed to make provider executable", zap.Error(err))
-		c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
-		c.Abort()
-		return
 	}
 }
