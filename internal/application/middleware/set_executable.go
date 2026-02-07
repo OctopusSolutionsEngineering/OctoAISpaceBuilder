41a8b6b5f416010090944100097960664a75d69a
Matthew Casperson <matthewcasperson@gmail.com>
2026-02-08 07:03:11 +1000

    Enhance error logging in MakeExecutable middleware to include install directory context

--- internal/application/middleware/set_executable.go
+++ internal/application/middleware/set_executable.go
@@ -1,13 +1,14 @@
 package middleware
 
 import (
+	"net/http"
+	"path/filepath"
+
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/application/responses"
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/environment"
 	"github.com/OctopusSolutionsEngineering/OctoAISpaceBuilder/internal/domain/execute"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
-	"net/http"
-	"path/filepath"
 )
 
 // MakeExecutable is a middleware function that sets the executable permissions for the specified files.
@@ -25,14 +26,14 @@ func MakeExecutable(c *gin.Context) {
 	}
 
 	if err := execute.MakeAllExecutable(filepath.Join(installDir, "binaries")); err != nil {
-		zap.L().Error("Failed to make binaries executable", zap.Error(err))
+		zap.L().Error("Failed to make binaries executable under "+installDir, zap.Error(err))
 		c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
 		c.Abort()
 		return
 	}
 
 	if err := execute.MakeAllExecutable(filepath.Join(installDir, "provider")); err != nil {
-		zap.L().Error("Failed to make provider executable", zap.Error(err))
+		zap.L().Error("Failed to make provider executable under "+installDir, zap.Error(err))
 		c.IndentedJSON(http.StatusInternalServerError, responses.GenerateError("Failed to process request", err))
 		c.Abort()
 		return
