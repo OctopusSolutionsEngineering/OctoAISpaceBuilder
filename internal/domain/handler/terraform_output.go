8ebaa80b618ae7efc80e81691e5d45083350917a
Matthew Casperson <matthewcasperson@gmail.com>
2026-03-02 06:37:43 +1000

    refactor: Rename comment for clarity on transient network error messages

--- internal/domain/handler/terraform_output.go
+++ internal/domain/handler/terraform_output.go
@@ -2,7 +2,7 @@ package handler
 
 import "strings"
 
-// These error message indicate a transient network issue that may be solved by retrying.
+// FlakyNetworkStrings defines error messages that indicate a transient network issue that may be solved by retrying.
 var FlakyNetworkStrings []string = []string{"network is unreachable", "handshake timeout"}
 
 func IsFlakyNetworkError(output string) bool {
